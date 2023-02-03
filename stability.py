ac7f55b39610c856f75755dd5d7639aa59150442
willianchu <willianchu@hotmail.com>
2023-02-03 15:42:14 -0800

    Stability initial setup

---
+++ stability.py
@@ -0,0 +1,40 @@
+import os
+import io
+import warnings
+from PIL import Image
+from stability_sdk import client
+import stability_sdk.interfaces.gooseai.generation.generation_pb2 as generation
+
+# Our Host URL should not be prepended with "https" nor should it have a trailing slash.
+os.environ['STABILITY_HOST'] = 'grpc.stability.ai:443'
+
+# Sign up for an account at the following link to get an API Key.
+# https://beta.dreamstudio.ai/membership
+
+# Click on the following link once you have created an account to be taken to your API Key.
+# https://beta.dreamstudio.ai/membership?tab=apiKeys
+
+# Paste your API Key below.
+
+os.environ['STABILITY_KEY'] = 'key-goes-here'
+
+## connection
+# Set up our connection to the API.
+stability_api = client.StabilityInference(
+    key=os.environ['STABILITY_KEY'], # API Key reference.
+    verbose=True, # Print debug messages.
+    engine="stable-diffusion-v1-5", # Set the engine to use for generation. 
+    # Available engines: stable-diffusion-v1 stable-diffusion-v1-5 stable-diffusion-512-v2-0 stable-diffusion-768-v2-0 
+    # stable-diffusion-512-v2-1 stable-diffusion-768-v2-1 stable-inpainting-v1-0 stable-inpainting-512-v2-0
+)
+
+# Set up our request.
+# Set up our connection to the API.
+stability_api = client.StabilityInference(
+    key=os.environ['STABILITY_KEY'], # API Key reference.
+    verbose=True, # Print debug messages.
+    engine="stable-diffusion-v1-5", # Set the engine to use for generation. 
+    # Available engines: stable-diffusion-v1 stable-diffusion-v1-5 stable-diffusion-512-v2-0 stable-diffusion-768-v2-0 
+    # stable-diffusion-512-v2-1 stable-diffusion-768-v2-1 stable-inpainting-v1-0 stable-inpainting-512-v2-0
+)
+
